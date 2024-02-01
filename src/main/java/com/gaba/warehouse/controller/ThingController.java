3cfacdc111b8b340ec77d9fa19eb3a114158a69f
Gabriela Ramírez <ramirezgabrielap@gmail.com>
2024-02-01 00:47:50 -0300

    Temas visuales

--- src/main/java/com/gaba/warehouse/controller/ThingController.java
+++ src/main/java/com/gaba/warehouse/controller/ThingController.java
@@ -2,25 +2,16 @@ package com.gaba.warehouse.controller;
 
 import com.gaba.warehouse.model.Thing;
 import com.gaba.warehouse.repository.ThingRepository;
-import jakarta.validation.Valid;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/thing")
@@ -33,91 +24,47 @@ public class ThingController {
         this.thingRepository = planetRepository;
     }
 
-    @GetMapping("/home")
-    public String Home(Model model) {
-            model.addAttribute("name", " dd ");
-            return "home";
-    }
-    
     @GetMapping("/list")
-    public String listThings(Model model) {
+    public String list(Model model) {
         List<Thing> things = thingRepository.findAll();
         model.addAttribute("things", things);
-        return "listThings";
+        return "list";
     }
     
-    @GetMapping("/{id}/edit")
-    public String showEditForm(@PathVariable int id, Model model) {
-        Thing thing = thingRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid thing ID"));
-        model.addAttribute("record", thing);
-        return "edit"; // Asegúrate de tener una vista llamada "edit"
+    @GetMapping("/create")
+    public String create(Model model) {
+        return "create"; 
     }
 
-    @GetMapping("/new")
-    public String showCreateForm(Model model) {
-        model.addAttribute("record", new Thing());
-        return "create"; // Asegúrate de tener una vista llamada "create"
-    }
-
-    @PostMapping("/create")
-    public String createRecord(@ModelAttribute("record") Thing newThing) {
-        thingRepository.save(newThing);
+    @PostMapping("/save")
+    public String save(@ModelAttribute("thing") Thing thing) {
+        thingRepository.save(thing);
         return "redirect:/thing/list";
     }
 
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable int id, Model model) {
+        Thing thing = thingRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid thing ID"));
+        model.addAttribute("thing", thing);
+        return "edit"; 
+    }
 
-    // Método para procesar la actualización del registro
+    
     @PostMapping("/update")
-    public String updateRecord(@ModelAttribute("record") Thing updatedThing) {
+    public String update(@ModelAttribute("thing") Thing updatedThing) {
         Thing thing = thingRepository.findById(updatedThing.getId()).orElseThrow(() -> new IllegalArgumentException("Invalid thing ID"));
         
-        // Actualizar solo los campos permitidos (name y amount)
         thing.setName(updatedThing.getName());
         thing.setAmount(updatedThing.getAmount());
 
         thingRepository.save(thing);
-        return "redirect:/thing/list"; // Redirige a la página de listado después de la actualización
+        return "redirect:/thing/list"; 
     }
     
-    @GetMapping("/{id}/delete")
+    @DeleteMapping("/{id}")
     public String delete(@PathVariable Integer id){
         thingRepository.deleteById(id);
         return "redirect:/thing/list";
     }
-    
-    
-    
-    /*
-    @GetMapping()
-    public Iterable<Thing> getAll(@PageableDefault(page = 0, size = 10) Pageable pageable) {
-        return thingRepository.findAll(Sort.by("id").ascending());
-    }
-    
-    @GetMapping("/{id}")
-    public Optional<Thing> get(@PathVariable Integer id) {
-        return this.thingRepository.findById(id);
-    }
-    
-    @PostMapping()
-    public Thing save(@Valid @RequestBody Thing thing){
-        return this.thingRepository.save(thing);
-    }
-    
-    @PutMapping("/{id}")
-    public Thing edit(@Valid @RequestBody Thing thing, @PathVariable Integer id){
-        thing.setId(id);
-        return this.thingRepository.save(thing);
-    }
-    
-    
-    @DeleteMapping("/{id}")
-    public void delete(@PathVariable Integer id){
-        this.thingRepository.deleteById(id);
-    }
-    
-    @GetMapping("/count")
-    public Long countPlanets(){
-        return thingRepository.count();
-    }
-*/
+
 }
